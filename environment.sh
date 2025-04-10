b90f78cc741c6ef8dade4831f80772330d293f06
Ricardo Botha <irmandos@users.noreply.github.com>
2025-04-10 10:35:37 +0200

    Create environment.sh

---
+++ environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build environment
+BASEDIR=$(dirname $(realpath ${0}))
+NETWDIR=="${BASEDIR}/pumba"
+NETWSHR=="//pumba.bothahome.co.za/webDavShare/Ubuntu/netboot-build"
+SOURCE_FILES="${BASEDIR}/files"
+IMG_FILE="${NETWDIR}/rootfs.img"
+ROOTFS_MNT="${NETWDIR}/rootfs.mnt"
+
+#Ensure we have the tools we need
+echo 'Acquire::http::Proxy "http://apt-cacher-ng.bothahome.co.za:3142";'>/etc/apt/apt.conf.d/00aptproxy
+apt install cifs-utils intiramfs-utils git wget nano 
+
+mkdir ${NETWDIR}
+mount -t cifs ${NETWDIR} ${NETWDIR} -o username=irmandos,uid=$(id -u),gid=$(id -g)
